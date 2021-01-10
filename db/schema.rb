7e0b70dd087066d2dff4af4353a205e6ab3d0010
Sherrpass <sherwinpoh3@gmail.com>
2021-01-10 19:47:49 +0800

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_06_070929) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
